403140648a29dce153c8e3c90f57ba50fb9632f1
yaejinpark <yaejinpark@Yae-Jins-MacBook-Pro.local>
2021-09-10 05:21:35 -0700

    deliverable 3 code finished

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -8,7 +8,11 @@ mechacar_df <- read.csv(file="MechaCar_mpg.csv")
 
 # Perform linear regression using the lm() function. 
 # In the lm() function, pass in all six variables
-mechacar_lr <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_df)
+mechacar_lr <- lm(mpg ~ vehicle_length + 
+                    vehicle_weight + 
+                    spoiler_angle + 
+                    ground_clearance + 
+                    AWD, data=mechacar_df)
 
 # Using the summary() function, 
 # determine the p-value and the r-squared value for the linear regression model.
@@ -23,5 +27,37 @@ sc_table <- read.csv(file="Suspension_Coil.csv")
 
 # Create a total_summary dataframe using the summarize() function to get the 
 # mean, median, variance, and standard deviation of the suspension coil’s PSI column.
-total_summary <- sc_table %>% summarize(psi_mean=mean(PSI), psi_med=median(PSI), psi_var=var(PSI), psi_sd=sd(PSI))
+total_summary <- sc_table %>% summarize(
+  psi_mean = mean(PSI), 
+  psi_med = median(PSI), 
+  psi_var = var(PSI), 
+  psi_sd = sd(PSI))
 
+# Create a lot_summary dataframe using the group_by() and the summarize() functions 
+# to group each manufacturing lot by the mean, median, variance, and standard deviation 
+# of the suspension coil’s PSI column.
+lot_summary <- sc_table %>% group_by(Manufacturing_Lot) %>% summarize(
+  psi_mean = mean(PSI), 
+  psi_med = median(PSI), 
+  psi_var = var(PSI), 
+  psi_sd = sd(PSI))
+
+################################################################################
+
+# Deliverable 3
+
+# Determine if the PSI across all manufacturing lots is statistically different from 
+# the population mean of 1,500 pounds per square inch.
+t.test(log10(sc_table$PSI), mu= mean(log10(sc_table$PSI)))
+
+# Determine if the PSI for each manufacturing lot is statistically different from 
+# the population mean of 1,500 pounds per square inch.
+
+# Lot 1
+t.test(log10(subset(sc_table, Manufacturing_Lot == "Lot1")$PSI), mu = mean(log10(sc_table$PSI)))
+
+# Lot 2
+t.test(subset(sc_table, Manufacturing_Lot == "Lot2")$PSI, mu = mean(log10(sc_table$PSI)))
+
+# Lot 3
+t.test(subset(sc_table, Manufacturing_Lot == "Lot3")$PSI, mu = mean(log10(sc_table$PSI)))
--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Yae Jin Park\
 Module 15 - Statistics and R
 
+## Overview
+## Linear Regression to Predict MPG
+## Summary Statistics on Suspension Coils
+## T-Tests on Suspension Coils
+## Study Design: MechaCar vs Competition
